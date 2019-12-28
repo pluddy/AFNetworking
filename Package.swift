30c8795e0a65931d81a91cbc9bca50ad62222ffc
Patrick Luddy <pluddy@outlook.com>
2019-12-28 10:50:34 -0600

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "AFNetworking",
+    products: [
+        .library(name: "AFNetworking", targets: ["AFNetworking"]),
+    ],
+    targets: [
+        .target(name: "AFNetworking")
+    ]
+)
